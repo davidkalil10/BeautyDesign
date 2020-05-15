6a2f076d146d8b1f97f3d5eead6d28d59d8d9ebb
davidkalil10 <dmmd10@hotmail.com>
2020-05-15 17:10:21 -0300

    Validação do primeiro formulario feita Mudança de componente para textformfield bugfix do custombuttom
    Falta: complementar o restante de validação dos campos, e melhorar as regras de validação

---
+++ lib/components/TextFieldFormularioTop.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+const Color rosaPaula = Color(0xffFA879E);
+
+class TextFieldFormularioTop extends StatelessWidget {
+
+  final TextEditingController controller;
+  final Icon icon;
+  final String labelText;
+  final String hintText;
+  final TextInputType keyboardType;
+  final List<TextInputFormatter> mascara;
+  final double padding;
+  final Function onSaved;
+  final Function validator;
+
+  TextFieldFormularioTop({
+    @required this.labelText,
+    @required this.hintText,
+    @required this.controller,
+    @required this.onSaved,
+    @required this.validator,
+    this.icon,
+    this.keyboardType,
+    this.mascara,
+    this.padding = 15,
+  });
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: padding),
+      child: TextFormField(
+        keyboardType: keyboardType,
+        controller: controller,
+        maxLines: 3,
+        minLines: 1,
+        style: TextStyle(fontSize: 20.0, color: Colors.black),
+        inputFormatters: mascara,
+        autofocus: false,
+        autocorrect: true,
+        autovalidate: true,
+        validator: validator,
+        onSaved: onSaved,
+        decoration: InputDecoration(
+          labelText: labelText,
+          labelStyle: TextStyle(fontSize: 20.0, color: rosaPaula),
+          hintText: hintText,
+          focusedBorder: UnderlineInputBorder(
+            //mudar cor da linha
+            borderSide: BorderSide(color: rosaPaula),
+          ),
+          icon: icon,
+        ),
+      ) ,
+    );
+  }
+}
---
+++ lib/components/cliente.dart
@@ -0,0 +1,11 @@
+class DadosCliente{
+  String nome ="";
+  String nascimento ="";
+  String telefone ="";
+  String instagram ="";
+  String email ="";
+  String atividade ="";
+  String cpf ="";
+  String endereco ="";
+  String comoConheceu ="";
+}
